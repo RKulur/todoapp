c29ce92d3c45d97dcb5cb48bd6441f19bb145a4c
Rashwith Kulur <110486028+RKulur@users.noreply.github.com>
2024-01-15 22:15:30 +0530

    Update displayTodos.ts

--- src/components/displayTodos.ts
+++ src/components/displayTodos.ts
@@ -4,8 +4,6 @@ import { Todo } from "./interface/interface";
 export default function displayTodos(element: Element, todos: Todo[]) {
   todos &&
     todos.forEach((todo) => {
-      console.log(todos.length);
-      console.log(todo);
       insertTodo(element, todo.todo, todo.todoId, todo.checked);
     });
 }

133c5e51b66ea99f05872fb10103f82630de16fc
Rashwith Kulur <110486028+RKulur@users.noreply.github.com>
2024-01-15 22:16:16 +0530

    Update deleteTodo.ts

--- src/components/deleteTodo.ts
+++ src/components/deleteTodo.ts
@@ -1,6 +1,5 @@
 export default function deleteTodo(element: Element, id: string) {
   const todoToBeDeleted = element.querySelector(`#${id}`) as HTMLDivElement;
   element.removeChild(todoToBeDeleted);
-  console.log(id);
   localStorage.removeItem(id.slice(2));
 }
